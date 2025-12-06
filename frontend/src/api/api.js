60abf7dbce04a974b0c4ebe0b61363b8de694f90
Mizan <comanfake@gmail.com>
2025-12-06 16:11:32 +0600

    Something Updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // deployed backend URL
-  withCredentials: true,                 // matches backend
+  baseURL: import.meta.env.VITE_API_URL, // https://monitor-r0u9.onrender.com/api
+  withCredentials: true,
 });
 
 export default api;
