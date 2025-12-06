205852a0d342557ac07b9ee520e77fe9c25e5b7b
Mizan <comanfake@gmail.com>
2025-12-06 11:37:18 +0600

    Something Updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json()); // Parse JSON bodies
-
+// app.use(cors({ origin: true })); // restrict origins in production
 app.use(
   cors({
     origin: [
